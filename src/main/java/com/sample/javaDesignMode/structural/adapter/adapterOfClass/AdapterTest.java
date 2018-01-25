8d0063ac75e9db56e9ffbbbbdbb5d8b6d83bd9eb
weiloo126 <weiloo126@126.com>
2018-01-25 10:05:31 +0800

    commit test

--- src/main/java/com/sample/javaDesignMode/structural/adapter/adapterOfClass/AdapterTest.java
+++ src/main/java/com/sample/javaDesignMode/structural/adapter/adapterOfClass/AdapterTest.java
@@ -2,7 +2,7 @@ package com.sample.javaDesignMode.structural.adapter.adapterOfClass;
 
 /**
  * 适配器模式将某个类的接口转换成客户端期望的另一个接口表示，目的是消除由于接口不匹配所造成的类的兼容性问题。主要分为三类：类的适配器模式、对象的适配器模式、接口的适配器模式。
- * 类的适配器模式 核心思想就是：有一个Source类，拥有一个方法，待适配，目标接口时Targetable，通过Adapter类，将Source的功能扩展到Targetable里
+ * 类的适配器模式 核心思想就是：有一个Source类，拥有一个方法，待适配，目标接口是Targetable，通过Adapter类，将Source的功能扩展到Targetable里
  * 
  * 总结一下三种适配器模式的应用场景：
  * 
